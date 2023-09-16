d727787192c591ddeec0774f8d944d721efea22d
Jimbonk Buchanon <jimbonkgit@gmail.com>
2023-09-16 07:19:00 -0400

    Modify nato
    * Convert NATO phrase to a characters
    * Search a file for NATO words and convert to chars

--- nato_output/nato.c
+++ nato_output/nato.c
@@ -1,9 +1,51 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define BUFFER_LENGTH 64
 
+char isterm(char *);
+void natoToWord();
+void wordToNato();
+void checkFile(char *);
+
 int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        if (strcmp(argv[1], "--nato-to-word") == 0) {
+            natoToWord();
+        } else if (strcmp(argv[1], "--file") == 0) {
+            if (argc < 3) {
+                fprintf(stderr, "Please supply a text file argument\n");
+                exit(1);
+            }
+            checkFile(argv[2]);
+        }
+    } else {
+        wordToNato();
+    }
+
+    return 0;
+}
+
+void natoToWord() {
+    char phrase[BUFFER_LENGTH];
+    char *match;
+    char ch;
+
+    printf("NATO word or phrase: ");
+    fgets(phrase, BUFFER_LENGTH, stdin);
+
+    match = strtok(phrase, " ");
+    while (match) {
+        if ((ch = isterm(match)) != '\0')
+            putchar(ch);
+        match = strtok(NULL, " "); // this hurts my soul
+    }
+    putchar('\n');
+}
+
+void wordToNato() {
     const char *nato[] = {"Alfa",    "Bravo",  "Charlie", "Delta",    "Echo",
                           "Foxtrot", "Golf",   "Hotel",   "India",    "Juliett",
                           "Kilo",    "Lima",   "Mike",    "November", "Oscar",
@@ -27,6 +69,61 @@ int main(int argc, char *argv[]) {
             break;
     }
     putchar('\n');
+}
 
-    return 0;
+void checkFile(char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Unable to open '%s'\n", filename);
+        exit(1);
+    }
+
+    const int MAX_WORD_LENGTH = 64;
+    char word[MAX_WORD_LENGTH];
+    int ch;
+    int offset = 0;
+
+    while ((ch = fgetc(file)) != EOF) {
+        if (isalpha(ch)) {
+            word[offset] = ch;
+            offset++;
+            if (offset >= MAX_WORD_LENGTH) {
+                fprintf(stderr, "Buffer overflow\n");
+                return;
+            }
+        } else if (offset > 0) {
+            word[offset] = '\0';
+            putchar(isterm(word));
+            offset = 0;
+        }
+    }
+    putchar('\n');
+    fclose(file);
+}
+
+char isterm(char *term) {
+    const char *nato[] = {"Alfa",    "Bravo",  "Charlie", "Delta",    "Echo",
+                          "Foxtrot", "Golf",   "Hotel",   "India",    "Juliett",
+                          "Kilo",    "Lima",   "Mike",    "November", "Oscar",
+                          "Papa",    "Quebec", "Romeo",   "Sierra",   "Tango",
+                          "Uniform", "Victor", "Whiskey", "Xray",     "Yankee",
+                          "Zulu"};
+    const char *n;
+    char *t;
+
+    for (int i = 0; i < 26; i++) {
+        n = nato[i];
+        t = term;
+        while (*n != '\0') {
+            if ((*n | 0x20) != (*t | 0x20))
+                break;
+            n++;
+            t++;
+        }
+
+        if (*n == '\0')
+            return *nato[i];
+    }
+
+    return '\0';
 }
